8fedfca0b205c6c4fe9fe13f9ab679b38f21ab3e
Angelo Bihis <anjo.bihis@gmail.com>
2021-01-03 09:58:14 +0800

    updated entry point, set up error handler and routes etc

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-
-//Route Files
-const bootcamps = require('./routes/bootcamps');
+const colors = require('colors');
+const errorHandler = require('./middleware/error');
+const connectDB = require('./config/db');
 
 //Load env vars
 dotenv.config({path: './config/config.env'});
 
+//Connect to database
+connectDB();
+
+//Route Files
+const bootcamps = require('./routes/bootcamps');
+
 const app = express();
+
+//Body Parser
+app.use(express.json());
 
 //Dev logging middleware
 if(process.env.NODE_ENV === 'development'){
@@ -18,6 +27,15 @@ if(process.env.NODE_ENV === 'development'){
 //Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 
+app.use(errorHandler);
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
+
+//Global "Unhandled Promise" Handler
+process.on('unhandledRejection', (err, promise) => {
+   console.log(`Error: ${err.message}`.red);
+   //Close server & exit process
+   server.close(() => process.exit(1));
+});
