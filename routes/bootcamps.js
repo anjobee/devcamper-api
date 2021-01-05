56975d38750f832d0b6d89ca93ad0ba2ecaf9a1d
Angelo Bihis <anjo.bihis@gmail.com>
2021-01-05 16:39:17 +0800

    working on reviews

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -13,6 +13,7 @@ const Bootcamp = require('../models/Bootcamp');
 
 //Include other resource routers
 const courseRouter = require('./courses');
+const reviewRouter = require('./reviews');
 
 const router = express.Router();
 
@@ -21,7 +22,9 @@ const { protect, authorize } = require('../middleware/auth');
 
 //Re-route into other resource routers
 router.use('/:bootcampId/courses', courseRouter);
+router.use('/:bootcampId/reviews', reviewRouter);
 
+//Routes
 router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
 
 router.route('/:id/photo').put(protect, authorize('publisher', 'admin'), bootcampPhotoUpload);
